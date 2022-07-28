4a8b1b59bffe22b44cebe9ff8c595fe5c1d7ad8a
Alexander <braylore27@gmail.com>
2022-07-28 13:51:52 +0300

    added constants

---
+++ src/constants/jsonServerUrl.ts
@@ -0,0 +1,1 @@
+export const jsonServerBaseUrl = 'http://localhost:3001';
---
+++ src/constants/routes.ts
@@ -0,0 +1,8 @@
+export enum Routes {
+  SHOP = '/',
+  ADMIN = 'admin',
+  BASKET = 'basket',
+  LOGIN = 'login',
+  REGISTRATION = 'registration',
+  PRODUCT = 'product'
+}
