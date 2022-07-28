e797d4f67c14526361d7350f0b7e38c4c2d52448
Alexander <braylore27@gmail.com>
2022-07-28 14:11:46 +0300

    IProducts interface

---
+++ src/types/IProducts.ts
@@ -0,0 +1,10 @@
+export interface IProducts {
+  id: string;
+  img: string;
+  title: string;
+  description: string;
+  author: string;
+  numberOfPages: number;
+  category: string[];
+  price: number;
+}
